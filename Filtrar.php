241dcb28e211ae9f527eeabd99b895a433c834a4
Joan735 <joanye777@gmail.com>
2025-04-30 10:20:33 +0200

    implementar Filtrar.php

---
+++ Filtrar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Filtrar Evento</title>
+</head>
+
+<body>
+  <h2>Filtrar Evento por ID</h2>
+  <form action="Filtrar.php" method="post">
+    <label>ID: <input type="text" name="id" required></label><br>
+    <br><br>
+    <button type="submit" name="submit">Borrar evento</button>
+  </form>
+</body>
+
+</html>
+
+<?php
+include_once 'load.php';
+
+use BaseXClient\BaseXException;
+use BaseXClient\Session;
+
+if (!isset($_POST['id'])) {
+  $_POST['id'] = "";
+}
+
+$id = htmlspecialchars($_POST['id']);
+
+if (isset($_POST['submit'])) {
+  try {
+    $session = new Session("localhost", 1984, "admin", "admin");
+    $session->execute("OPEN eventos"); // obrim la base de dades
+
+    // Borrar evento si se encuentra el id
+    $xquerySort = <<< XQ
+XQUERY
+  for \$a in //evento
+  where \$a/id="$id"
+  return \$a
+
+XQ;
+
+    $evento = $session->execute($xquerySort);
+    if ($evento != "") {
+      echo "<p>✅ Evento encontrado correctamente con ID $id.</p>";
+      echo "<h3>📋 Evento:</h3>";
+      echo "<pre>" . htmlspecialchars($evento) . "</pre>";
+    } else {
+      echo "<p>❌ Evento no encontrado con ID $id.</p>";
+    }
+
+    $session->close();
+  } catch (Exception $e) {
+    echo "❌ Error: " . $e->getMessage();
+  }
+}
+?>
